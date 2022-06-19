e9d08312f699765891afc11ce459e914687e55cc
Andyauk <47583709+Andyauk@users.noreply.github.com>
2022-06-19 19:48:59 +0100

    clean up of commented out code

--- client/main.lua
+++ client/main.lua
@@ -469,143 +469,6 @@ RegisterCommand('closeinv', function()
     closeInventory()
 end, false)
 
--- RegisterCommand('inventory', function()
-    -- if not isCrafting and not inInventory then
-        -- if not PlayerData.metadata["isdead"] and not PlayerData.metadata["inlaststand"] and not PlayerData.metadata["ishandcuffed"] and not IsPauseMenuActive() then
-            -- local ped = PlayerPedId()
-            -- local curVeh = nil
-            -- local VendingMachine = GetClosestVending()
-
-            -- if IsPedInAnyVehicle(ped) then -- Is Player In Vehicle
-                -- local vehicle = GetVehiclePedIsIn(ped, false)
-                -- CurrentGlovebox = exports['qbr-core']:GetPlate(vehicle)
-                -- curVeh = vehicle
-                -- CurrentVehicle = nil
-            -- else
-                -- local vehicle = exports['qbr-core']:GetClosestVehicle()
-                -- if vehicle ~= 0 and vehicle ~= nil then
-                    -- local pos = GetEntityCoords(ped)
-                    -- local trunkpos = GetOffsetFromEntityInWorldCoords(vehicle, 0, -2.5, 0)
-                    -- if (IsBackEngine(GetEntityModel(vehicle))) then
-                        -- trunkpos = GetOffsetFromEntityInWorldCoords(vehicle, 0, 2.5, 0)
-                    -- end
-                    -- if #(pos - trunkpos) < 2.0 and not IsPedInAnyVehicle(ped) then
-                        -- if GetVehicleDoorLockStatus(vehicle) < 2 then
-                            -- CurrentVehicle = exports['qbr-core']:GetPlate(vehicle)
-                            -- curVeh = vehicle
-                            -- CurrentGlovebox = nil
-                        -- else
-                            -- exports['qbr-core']:Notify(9, "Vehicle Locked", 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
-                            -- return
-                        -- end
-                    -- else
-                        -- CurrentVehicle = nil
-                    -- end
-                -- else
-                    -- CurrentVehicle = nil
-                -- end
-            -- end
-
-            -- if CurrentVehicle then -- Trunk
-                -- local vehicleClass = GetVehicleClass(curVeh)
-                -- local maxweight = 0
-                -- local slots = 0
-                -- if vehicleClass == 0 then
-                    -- maxweight = 38000
-                    -- slots = 30
-                -- elseif vehicleClass == 1 then
-                    -- maxweight = 50000
-                    -- slots = 40
-                -- elseif vehicleClass == 2 then
-                    -- maxweight = 75000
-                    -- slots = 50
-                -- elseif vehicleClass == 3 then
-                    -- maxweight = 42000
-                    -- slots = 35
-                -- elseif vehicleClass == 4 then
-                    -- maxweight = 38000
-                    -- slots = 30
-                -- elseif vehicleClass == 5 then
-                    -- maxweight = 30000
-                    -- slots = 25
-                -- elseif vehicleClass == 6 then
-                    -- maxweight = 30000
-                    -- slots = 25
-                -- elseif vehicleClass == 7 then
-                    -- maxweight = 30000
-                    -- slots = 25
-                -- elseif vehicleClass == 8 then
-                    -- maxweight = 15000
-                    -- slots = 15
-                -- elseif vehicleClass == 9 then
-                    -- maxweight = 60000
-                    -- slots = 35
-                -- elseif vehicleClass == 12 then
-                    -- maxweight = 120000
-                    -- slots = 35
-                -- elseif vehicleClass == 13 then
-                    -- maxweight = 0
-                    -- slots = 0
-                -- elseif vehicleClass == 14 then
-                    -- maxweight = 120000
-                    -- slots = 50
-                -- elseif vehicleClass == 15 then
-                    -- maxweight = 120000
-                    -- slots = 50
-                -- elseif vehicleClass == 16 then
-                    -- maxweight = 120000
-                    -- slots = 50
-                -- else
-                    -- maxweight = 60000
-                    -- slots = 35
-                -- end
-                -- local other = {
-                    -- maxweight = maxweight,
-                    -- slots = slots,
-                -- }
-                -- TriggerServerEvent("inventory:server:OpenInventory", "trunk", CurrentVehicle, other)
-                -- OpenTrunk()
-            -- elseif CurrentGlovebox then
-                -- TriggerServerEvent("inventory:server:OpenInventory", "glovebox", CurrentGlovebox)
-            -- elseif CurrentDrop then
-                -- TriggerServerEvent("inventory:server:OpenInventory", "drop", CurrentDrop)
-            -- elseif VendingMachine then
-                -- local ShopItems = {}
-                -- ShopItems.label = "Vending Machine"
-                -- ShopItems.items = Config.VendingItem
-                -- ShopItems.slots = #Config.VendingItem
-                -- TriggerServerEvent("inventory:server:OpenInventory", "shop", "Vendingshop_"..math.random(1, 99), ShopItems)
-            -- else
-                -- openAnim()
-                -- TriggerServerEvent("inventory:server:OpenInventory")
-            -- end
-        -- end
-    -- end
--- end)
-
--- RegisterKeyMapping('inventory', 'Open Inventory', 'keyboard', 'TAB')
-
--- RegisterCommand('hotbar', function()
-    -- isHotbar = not isHotbar
-    -- if not PlayerData.metadata["isdead"] and not PlayerData.metadata["inlaststand"] and not PlayerData.metadata["ishandcuffed"] and not IsPauseMenuActive() then
-        -- ToggleHotbar(isHotbar)
-    -- end
--- end)
-
--- RegisterKeyMapping('hotbar', 'Toggles keybind slots', 'keyboard', 'z')
-
--- for i = 1, 6 do
-    -- RegisterCommand('slot' .. i,function()
-        -- if not PlayerData.metadata["isdead"] and not PlayerData.metadata["inlaststand"] and not PlayerData.metadata["ishandcuffed"] and not IsPauseMenuActive() then
-            -- if i == 6 then
-                -- i = MaxInventorySlots
-            -- end
-            -- TriggerServerEvent("inventory:server:UseItemSlot", i)
-        -- end
-    -- end)
-    -- RegisterKeyMapping('slot' .. i, 'Uses the item in slot ' .. i, 'keyboard', i)
--- end
-
 CreateThread(function()
     while true do
         Wait(0)
@@ -628,9 +491,6 @@ CreateThread(function()
 						if vehicle ~= 0 and vehicle ~= nil then
 							local pos = GetEntityCoords(ped)
 							local trunkpos = GetOffsetFromEntityInWorldCoords(vehicle, 0, -2.5, 0)
-							-- if (IsBackEngine(GetEntityModel(vehicle))) then
-							--     trunkpos = GetOffsetFromEntityInWorldCoords(vehicle, 0, 2.5, 0)
-							-- end
 							if #(pos - trunkpos) < 2.0 and not IsPedInAnyVehicle(ped) then
 								if GetVehicleDoorLockStatus(vehicle) < 2 then
 									CurrentVehicle = exports['qbr-core']:GetPlate(vehicle)
@@ -887,10 +747,6 @@ end)
 RegisterNUICallback("SetInventoryData", function(data)
     TriggerServerEvent("inventory:server:SetInventoryData", data.fromInventory, data.toInventory, data.fromSlot, data.toSlot, data.fromAmount, data.toAmount)
 end)
-
--- RegisterNUICallback("PlayDropSound", function()
-    -- PlaySound(-1, "CLICK_BACK", "WEB_NAVIGATION_SOUNDS_PHONE", 0, 0, 1)
--- end)
 
 RegisterNUICallback("PlayDropFail", function()
     PlaySound(-1, "Place_Prop_Fail", "DLC_Dmod_Prop_Editor_Sounds", 0, 0, 1)
