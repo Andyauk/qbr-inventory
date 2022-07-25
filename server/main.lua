83ecb027a8d641ccd271565b70a42cccb885eaab
Andyauk <47583709+Andyauk@users.noreply.github.com>
2022-07-25 14:38:50 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -6,6 +6,7 @@ local ShopItems = {}
 local sharedItems = exports['qbr-core']:GetItems()
 -- Functions
 
+
 local function recipeContains(recipe, fromItem)
 	for k, v in pairs(recipe.accept) do
 		if v == fromItem.name then
